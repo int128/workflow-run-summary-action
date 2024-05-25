088fcf3b15a86679f0a53bcf6ee1cbc29caa8ad4
Hidetake Iwata <int128@gmail.com>
2024-05-25 21:38:11 +0900

    Remove @octokit/webhooks-types

--- src/run.ts
+++ src/run.ts
@@ -1,6 +1,6 @@
+import assert from 'assert'
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { WorkflowRunEvent } from '@octokit/webhooks-types'
 import { getWorkflowRunSummary } from './workflow-run.js'
 import { getWorkflowRun } from './queries/workflow-run.js'
 
@@ -25,30 +25,27 @@ type AssociatedPullRequest = {
 
 export const run = async (inputs: Inputs): Promise<Outputs | undefined> => {
   const octokit = github.getOctokit(inputs.token)
+  const workflowRun = await getWorkflowRunForEvent(octokit)
+  return getWorkflowRunSummary(workflowRun)
+}
+
+const getWorkflowRunForEvent = async (octokit: Octokit) => {
   if (github.context.eventName === 'workflow_run') {
-    const e = github.context.payload as WorkflowRunEvent
-    return await handleWorkflowRun(e, octokit)
+    const workflowRun: unknown = github.context.payload.workflow_run
+    assert(typeof workflowRun === 'object')
+    assert(workflowRun !== null)
+    assert('node_id' in workflowRun)
+    assert(typeof workflowRun.node_id === 'string')
+    core.info(`Getting the target workflow run ${workflowRun.node_id}`)
+    return await getWorkflowRun(octokit, { id: workflowRun.node_id })
   }
-  return handleOtherEvent(octokit)
-}
 
-const handleWorkflowRun = async (e: WorkflowRunEvent, octokit: Octokit): Promise<Outputs> => {
-  core.info(`Getting the workflow run ${e.workflow_run.node_id}`)
-  const checkSuite = await getWorkflowRun(octokit, { id: e.workflow_run.node_id })
-  core.info(`Found the workflow run: ${JSON.stringify(checkSuite, undefined, 2)}`)
-  return getWorkflowRunSummary(checkSuite)
-}
-
-const handleOtherEvent = async (octokit: Octokit): Promise<Outputs> => {
-  core.info(`Getting the workflow run ${github.context.runId}`)
+  core.info(`Getting the current workflow run ${github.context.runId}`)
   const { data: workflowRun } = await octokit.rest.actions.getWorkflowRun({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
     run_id: github.context.runId,
   })
-
   core.info(`Getting the workflow run ${workflowRun.node_id}`)
-  const checkSuite = await getWorkflowRun(octokit, { id: workflowRun.node_id })
-  core.info(`Found the workflow run: ${JSON.stringify(checkSuite, undefined, 2)}`)
-  return getWorkflowRunSummary(checkSuite)
+  return await getWorkflowRun(octokit, { id: workflowRun.node_id })
 }
