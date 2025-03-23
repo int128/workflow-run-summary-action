1d3cae9600fd1e29196033d9c485504a8829ced9
Hidetake Iwata <int128@gmail.com>
2025-03-23 13:51:39 +0900

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 import { run } from './run.js'
 import { getContext, getOctokit } from './github.js'
 
-try {
+const main = async () => {
   const outputs = await run(getOctokit(), await getContext())
   core.setOutput('annotation-messages', outputs.annotationMessages)
   core.setOutput('annotation-failure-messages', outputs.annotationFailureMessages)
@@ -13,6 +13,10 @@ try {
     core.setOutput('pull-request-number', outputs.associatedPullRequest.number)
     core.setOutput('pull-request-url', outputs.associatedPullRequest.url)
   }
+}
+
+try {
+  await main()
 } catch (e) {
   core.setFailed(e instanceof Error ? e : String(e))
   console.error(e)
